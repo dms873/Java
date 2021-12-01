2e8c3b60b4a285c9946d290d36f60bbaeb8c3c49
Force <Force@Jinny>
2021-12-01 22:18:45 +0900

    git 이클립스 연동 test

---
+++ src/Hello.java
@@ -0,0 +1,20 @@
+
+public class Hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello, world");
+		
+		// ctrl+shift+L 단축키 전체 목록보기
+		// ctrl+ +,- 글자 확대 및 축소
+		// ctrl+D 한 줄 삭제
+		// ctrl+alt+down 행 단위 복사
+		// alt+shift+A 멀티컬럼 편집
+		// alt+up, down 행 단위 이동
+		// ctrl+i 자동 들여쓰기
+		// ctrl+/ 주석(토글)
+		// ctrl+space 자동완성
+		
+	}
+
+}
