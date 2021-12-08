95b7644b0b24fb2f75436453cb278fd877e9fe98
jinny <dms873@naver.com>
2021-12-08 16:36:48 +0900

    Java Practice

--- src/practice/operator/OperatorPractice.java
+++ src/practice/operator/OperatorPractice.java
@@ -1,5 +1,7 @@
 package practice.operator;
 
+import java.util.Scanner;
+
 public class OperatorPractice {
 
 	public void method1() {
@@ -17,5 +19,19 @@ public class OperatorPractice {
 		System.out.println("num1 : " + num1);
 		System.out.println("num2 : " + num2);
 		System.out.println("str : " + str);
+	}
+	public void method2() {
+		// 문제 2 삼항연산자 두 개의 정수의 짝홀 판별
+		Scanner sc = new Scanner(System.in);
+		System.out.print("첫 번째 정수 입력 : ");
+		int num1 = sc.nextInt();
+		System.out.print("두 번째 정수 입력 : ");
+		int num2 = sc.nextInt();
+		
+		String result = (num1 % 2 == 0) && (num2 % 2 == 0) ? "두 수 모두 짝수이다." : 
+						(num1 % 2 == 0) || (num2 % 2 == 0) ? "하나는 홀수이고 하나는 짝수이다." :
+							"두 수 모두 홀수이다";
+		
+		System.out.println(result);
 	}
 }
