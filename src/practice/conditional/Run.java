ce9fb02fe1a0f8ed7aa5688fddaa9e0417dc199f
jinny <dms873@naver.com>
2021-12-09 13:14:36 +0900

    Java Practice

---
+++ src/practice/conditional/Run.java
@@ -0,0 +1,11 @@
+package practice.conditional;
+
+public class Run {
+
+	public static void main(String[] args) {
+		ConditionalPractice cp = new ConditionalPractice();
+//		cp.method1();
+		cp.method2();
+	}
+
+}
