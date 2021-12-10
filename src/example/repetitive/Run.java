3e165a639c6b68ba61e99161bbb998a0f7f21d6d
jinny <dms873@naver.com>
2021-12-10 12:29:12 +0900

    Java Practice

---
+++ src/example/repetitive/Run.java
@@ -0,0 +1,13 @@
+package example.repetitive;
+
+public class Run {
+
+	public static void main(String[] args) {
+		Repetitive re = new Repetitive();
+//		re.testFor();
+//		re.testWhile();
+//		re.testDowhile();
+		re.testGugudan();
+	}
+
+}
