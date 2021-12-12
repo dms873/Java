ff5faca6ff2c132cde1aa0506cbb1402e412c0c0
jinny <dms873@naver.com>
2021-12-13 00:32:32 +0900

    Java practice

---
+++ src/practice/demensionArray/Run.java
@@ -0,0 +1,12 @@
+package practice.demensionArray;
+
+public class Run {
+
+	public static void main(String[] args) {
+		DemensionArrayPractice dp = new DemensionArrayPractice();
+//		dp.method1();
+		dp.method2();
+
+	}
+
+}
