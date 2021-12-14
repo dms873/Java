2187a786f84d7e0e0b8df65554bf5578534f0775
jinny <dms873@naver.com>
2021-12-14 13:33:27 +0900

    Java Practice

---
+++ src/practice/comprehensive/Run.java
@@ -0,0 +1,11 @@
+package practice.comprehensive;
+
+public class Run {
+
+	public static void main(String[] args) {
+		ComprehensivePractice cp = new ComprehensivePractice();
+		cp.method();
+
+	}
+
+}
