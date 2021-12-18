fad623856e1ddf9832750e76585c9248e33c73d5
jinny <dms873@naver.com>
2021-12-18 23:58:05 +0900

    Java practice

---
+++ src/khoop1day03exam/Run.java
@@ -0,0 +1,10 @@
+package khoop1day03exam;
+
+
+public class Run {
+	public static void main(String[] args) {
+		// 클래스 변수는 사용하려고하면 클래스명을 써주고
+		// .을 입력 후에 해당 클래스 변수, 메소드를 사용할 수 있다.
+		System.out.println(FieldExam.var1);
+	}
+}
