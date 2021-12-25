790a651aa5b4f428be9395b3323b18bc76709d76
dms873 <83773369+dms873@users.noreply.github.com>
2021-12-25 14:14:12 +0900

    Java Practice

---
+++ javajungsuk/FlowEx12.java
@@ -0,0 +1,18 @@
+package javajungsuk;
+
+public class FlowEx12 {
+
+	public static void main(String[] args) {
+		for(int i = 1; i <= 5; i++) {
+			System.out.println(i); // i의 값을 출력한다.
+		}
+		
+		for(int i = 1; i <= 5; i++) {
+			System.out.print(i);   // 가로로 출력된다.
+		}
+		
+		System.out.println();
+
+	}
+
+}
