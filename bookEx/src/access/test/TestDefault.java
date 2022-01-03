bfe05afde04f4ea9314e186a4912010781f8525d
Jinny <83773369+dms873@users.noreply.github.com>
2022-01-03 17:28:51 +0900

    Practice variables in inheritance

---
+++ bookEx/src/access/test/TestDefault.java
@@ -0,0 +1,13 @@
+package access.test;
+
+import access.sample.DefaultSample1;
+
+public class TestDefault {
+	public static void main(String[] args) {
+		DefaultSample1 dt1 = new DefaultSample1();
+		DefaultSample2 dt2 = new DefaultSample2();
+		
+//		dt1.num1 = 100; // 다른 패키지의 클래스에서는 접근 불가능
+		dt2.num2 = 100; // 같은 패키지의 클래스에서는 접근 가능
+	}
+}
