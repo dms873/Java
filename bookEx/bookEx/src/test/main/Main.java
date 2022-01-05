513471b98db2461bb52433e6a4aefc651c6b8c99
Jinny <83773369+dms873@users.noreply.github.com>
2022-01-05 11:58:47 +0900

    Example practice

---
+++ bookEx/bookEx/src/test/main/Main.java
@@ -0,0 +1,14 @@
+package test.main;
+
+import test.math.MathTest;
+import test.random.RandomEx;
+import test.str.StringEx;
+
+public class Main {
+	public static void main(String[] args) {
+//		new StringEx().testString();
+//		new MathTest().printMath();
+		new RandomEx().testRandom();
+	}
+
+}
